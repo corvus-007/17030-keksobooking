331802a16d9ef37621a5bef40c5c98996f6a122e
Денис <corvus007@yandex.ru>
2017-09-04 18:41:19 +0300

    Перетаскивание пина в файле pin.js

--- js/pin.js
+++ js/pin.js
@@ -3,6 +3,7 @@
 window.pin = (function () {
   var dialog = document.querySelector('#offer-dialog');
   var tokioPinMap = document.querySelector('.tokyo__pin-map');
+  var mainPin = document.querySelector('.pin__main');
 
   var toggleActivePin = function (event) {
     var targetElement = event.target;
@@ -48,6 +49,31 @@ window.pin = (function () {
     return pinsFragment;
   };
 
+  var startCoords = {};
+
+  var onMouseMove = function (event) {
+    var shift = {
+      x: startCoords.x - event.clientX,
+      y: startCoords.y - event.clientY
+    };
+
+    startCoords.x = event.clientX;
+    startCoords.y = event.clientY;
+
+    mainPin.style.left = (mainPin.offsetLeft - shift.x) + 'px';
+    mainPin.style.top = (mainPin.offsetTop - shift.y) + 'px';
+
+    window.form.setNoticeAddress();
+  };
+  var onMouseUp = function () {
+    document.removeEventListener('mousemove', onMouseMove);
+    document.removeEventListener('mouseup', onMouseUp);
+  };
+  var onMouseOut = function () {
+    document.removeEventListener('mousemove', onMouseMove);
+    document.removeEventListener('mouseup', onMouseUp);
+  };
+
   tokioPinMap.appendChild(generatePins(window.data.ads));
   tokioPinMap.addEventListener('click', function (event) {
     toggleActivePin(event);
@@ -55,4 +81,36 @@ window.pin = (function () {
   tokioPinMap.addEventListener('keydown', function (event) {
     window.util.isEnterEvent(event, toggleActivePin.bind(event));
   });
+  mainPin.addEventListener('mousedown', function (event) {
+    var targetElement = event.target;
+
+    while (targetElement !== mainPin) {
+      if (targetElement === mainPin) {
+        break;
+      }
+
+      targetElement = targetElement.parentNode;
+    }
+
+    startCoords = {
+      x: event.clientX,
+      t: event.clientY
+    };
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+    document.addEventListener('mouseout', onMouseOut);
+  });
+
+  return {
+    getMainPinCoords: function () {
+      var MAIN_PIN_WIDTH = 75;
+      var MAIN_PIN_HEIGHT = 94;
+
+      return {
+        x: mainPin.offsetLeft + MAIN_PIN_WIDTH / 2,
+        y: mainPin.offsetTop + MAIN_PIN_HEIGHT
+      };
+    }
+  };
 })();
